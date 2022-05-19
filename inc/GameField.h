3c07c23f5beef88ddec0f490abf766292036b10c
Hong sunghyun <ghdtjdgus14@gmail.com>
2022-05-20 01:34:01 +0900

    make generate_field func

--- inc/GameField.h
+++ inc/GameField.h
@@ -3,8 +3,11 @@
 
 # include <iostream>
 # include <string>
+# include <cassert>
 
+# include <cstring>
 # include <fstream>
+# include <vector>
 
 using namespace std;
 
@@ -14,22 +17,28 @@ class GameField
 		int** field;
 		const int row_;
 		const int col_;
+
+		void read_map(vector<string>& map);
 	public :
 		GameField(int row_size, int col_size);
 
 		~GameField();
 
-		GameField(const GameField &cp);
+		GameField(const GameField& cp);
 
-		int init_map(string file_name);
-
-		int getCell(int x, int y) const;
+		int get_cell(int x, int y) const;
 		
-		void setCell(int x, int y, int value);
+		void set_cell(int x, int y, int value);
+
+		int get_row_size() const;
+
+		int get_col_size() const;
 
 		friend ostream& operator<< (ostream& os, const GameField& gf);
+		friend GameField generate_field(const string& file_name);
 };
 
 ostream& operator<< (ostream& os, const GameField& gf);
+GameField generate_field(const string& file_name);
 
 #endif
--- src/GameField.cpp
+++ src/GameField.cpp
@@ -25,32 +25,24 @@ GameField::GameField(const GameField &cp) : row_(cp.row_), col_(cp.col_)
 	}
 }
 
-int GameField::init_map(string file_name)
-{
-	string line;
-	ifstream file(file_name);
-	
-	if (!file.is_open())
-		return 0;
-	for (int i = 0; i < col_; i++)
-	{
-		if (!getline(file, line))
-			return 0;
-		for (int j = 0; j < row_; j++)
-			field[i][j] = line[j] - '0';
-	}
-	file.close();
-	return 1;
-}
-
-int GameField::getCell(int x, int y) const
+int GameField::get_cell(int x, int y) const
 {
 	return field[y][x];
 }
 
-void GameField::setCell(int x, int y, int value)
+void GameField::set_cell(int x, int y, int value)
 {
 	field[y][x] = value;
+}
+
+int GameField::get_row_size() const
+{
+	return row_;
+}
+
+int GameField::get_col_size() const
+{
+	return col_;
 }
 
 ostream& operator<< (ostream& os, const GameField& gf)
@@ -62,4 +54,47 @@ ostream& operator<< (ostream& os, const GameField& gf)
 		os << endl;
 	}
 	return os;
+}
+
+void GameField::read_map(vector<string>& map)
+{
+	for (int i = 0; i < col_; ++i)
+	{
+		for (int j = 0; j < row_; ++j)
+		{
+			if (strchr("01234", map[i][j]))
+				field[i][j] = map[i][j] - '0';
+			else
+				field[i][j] = -1;
+		}
+	}
+}
+
+GameField generate_field(const string& file_name)
+{
+	string line;
+	ifstream file(file_name);
+
+	vector<string> tmp;
+	int size_w(0);
+	int size_h(0);
+
+	//open and read mapfile
+	assert(file.is_open());
+	while (getline(file, line))
+	{
+		if (line.length() > size_w)
+			size_w = line.length();
+		tmp.emplace_back(line);
+		size_h++;
+	}
+	//make GameField
+	GameField gf(size_w, size_h);
+	//write field
+	gf.read_map(tmp);
+	//clean
+	tmp.clear();
+	vector<string>().swap(tmp);
+	file.close();
+	return gf;
 }
