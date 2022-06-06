b9a6ca9124f60bdee3db59857d000dc9eea44a2d
Hong sunghyun <ghdtjdgus14@gmail.com>
2022-06-07 02:08:55 +0900

    fix Point.h header

--- inc/Point.h
+++ inc/Point.h
@@ -14,11 +14,11 @@ public:
 
     Point moveTo(const int dir);
     bool isValid(GameField& gf);
+    
     friend bool operator==(const Point& x, const Point& y);
     Point& operator=(const Point& a);
     
     Point& operator+= (const Point& a);
-    friend Point operator+ (Point p1, const Point& p2);
     Point& operator-= (const Point& a);
     friend Point operator+ (Point p1, const Point& p2);
     friend Point operator- (Point p1, const Point& p2);
@@ -26,7 +26,10 @@ public:
     friend std::ostream& operator<<(std::ostream& outStream, const Point& point);
 };
 
+bool operator==(const Point& x, const Point& y);
 Point operator+ (Point p1, const Point& p2);
 Point operator- (Point p1, const Point& p2);
+
+std::ostream& operator<<(std::ostream& outStream, const Point& point);
 
 #endif
